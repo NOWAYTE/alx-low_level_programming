b395e29753e53762f3a14797c5de7c39641235c8
Daniel <danielkamaunganga3@gmail.com>
2023-02-16 16:07:45 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - a program that prints a line 
- * return 0 means success
+ * return:0 means success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
