733c6c4d0d510919140a5549c9a82a4beecfe917
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-07 03:33:35 -0700

    a function that prints a string followed by a new line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stddef.h>
+int _putchar(char s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _int_strlen_recursion(char *s);
