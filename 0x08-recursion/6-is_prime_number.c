7eaf4301f73f33edde4f2e6679e81b4fe850e69c
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-09 01:35:38 +0300

    A function thar returns 1 if the input is a prime number otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * is_prime_number - function that returns 1 if the input integer is a prime number
- * @n - integer parameter
+ * is_prime_number - function that returns 1
+ * @n: integer parameter
  *
  * Return: value;
  */
+
 int is_prime_number(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		return (0);
 	}
@@ -15,7 +16,7 @@ int is_prime_number(int n)
 	return (is_prime_number_helper(n, 2));
 }
 /**
- * is_prime_number - function that checks if prime
+ * is_prime_number_helper - function that checks if prime
  * @a: integer parameter
  * @b: integer parameter
  *
@@ -28,12 +29,12 @@ int is_prime_number_helper(int a, int b)
 		return (0);
 	}
 	else
-       	{
+	{
 		if (a == b)
 		{
 			return (1);
 		}
 	}
 
-	return(is_prime_number_helper(a, b + 1));
+	return (is_prime_number_helper(a, b + 1));
 }
