9983fdda915d57f64128b1de79136ef193fb95c9
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-11 17:50:31 +0300

    A program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
- * main -Entry to the program
- *
- * @argc: Argument count
+ * main -Entry to the main
  * @argv: Argument vector
- *
+ * @argc: Argument count
+ * @param __attribute__((unused)): Macro
  * Return: Always 0 (success)
  */
-int main(int argc __attribute((unused)), char *argv[])
+
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s", argv[0]);
 	printf("\n");
