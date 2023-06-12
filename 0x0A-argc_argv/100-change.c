6ff0a085c419e7be9c65a52f8bd94061172ce831
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-12 16:02:25 +0300

    A program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int r, y;
+	int r;
+	int y = 0;
 
 	if (argc != 1)
 	{
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include<stdio.h>
-int (char *arg);
+int checker(char *arg);
 int _putchar(char s);
 
 #endif
