d84cc0b81647c28c6244c343ba3d8b7c1cc02f42
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-12 23:32:35 +0300

    function that copies into a newly space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,4 +3,30 @@
  * _strdup - function that returns a newly allocated space
  * @str: character pointer parameter
  *
- * Return: 
+ * Return: Always 0 (success)
+ */
+char *_strdup(char *str)
+{
+
+	char *ptr;
+	int i;
+	int array;
+	int x;
+	char *mem;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		ptr[i] = str;
+
+		array  = i;
+	}
+
+	mem = malloc(sizeof(char) * array);
+
+	for (x = 0; i < array; i++)
+	{
+		mem[x] = ptr[x];
+	}
+
+	return (&mem);
+}
