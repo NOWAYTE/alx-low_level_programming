ae4e9a55d2e0f848d340878b488a75d85a7b035d
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-12 13:43:15 +0300

    A program that mulptiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry to the program 
+ * @argc: Argument count 
+ * @argv: Argument vector 
+ *
+ * Return: Always 0 (success)
+ * 
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int mul;
+
+	if (argc < 1)
+	{
+		printf("Error");
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	mul = x * y;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
+
