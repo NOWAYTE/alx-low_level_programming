fa9087e91c7608dba716b1dbb61be548d4a29bed
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-12 23:10:29 +0300

    A function that creates an array of strings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,6 +25,7 @@ char *create_array(unsigned int size, char c)
 	while (i < size)
 	{
 		ptr[i] = c;
+		i++;
 	}
 
 	return (ptr);
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * _strdup - function that returns a newly allocated space
+ * @str: character pointer parameter
+ *
+ * Return: 
