79a0bab1620357c7349268e893dd9167415a0241
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-13 21:48:38 +0300

    A function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str-concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * str_concat - function that concatenates two strings 
+ * @s1: character pointer parameter 
+ * @s2: pointer to a character parameter
+ *
+ * Return: return pointer to a newly allocated space
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str_1;
+	char *str_2;
+	char *ptr;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		str_1[i] = s1;
+
+		x = i;
+	}
+	
+	for (i = 0; i != '\0'; i++)
+	{
+		str_2 = s2;
+		
+		y = i;
+	}
+	z = x + y;
+
+	ptr = malloc(sizeof(char) * z + 1);
+	
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i != '\0'; i++)
+		{
+			ptr[i] = str_1[i];
+		}
+
+		for (i = 0; i!= '\0'; i++)
+		{
+			ptr[i] += str_2[i];
+		}
+	}
+
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<stddef.h>
 #include<stdlib.h>
-
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 
