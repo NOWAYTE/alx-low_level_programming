57e7b24f7f9c82332e6aff8c8fd7aa7f80e491f4
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-19 23:09:40 +0300

    Code update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name= %s\n", (*d).name);
+		printf("Name: %s\n", (*d).name);
 	}
 
 	if ((*d).age == 0)
@@ -36,7 +36,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Owner= %s\n", ((*d).owner));
+		printf("Owner: %s\n", ((*d).owner));
 
 	}
 
