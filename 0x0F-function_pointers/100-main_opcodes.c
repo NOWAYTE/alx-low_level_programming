92e3537c1ce47bc2ddbdbd1d2e5809321c9ead59
Daniel kamau <danielkamaunganga3@gmail.com>
2023-06-22 01:01:46 +0300

    A program that prints the opcodes of its main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,4 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
+	int i;
+	unsigned char x;
+	int y;
+
+	if (argc != 2)
+	{
+
+		printf("Error \n");
+		exit(1);
+
+	}
+
+	i = atoi(argv[1]);
+
+	x = (unsigned char*)main;
+
+	for (y = 0; y < i; i ++)
+	{
+		printf("%02x", *(x + y));
+	}
+
+	return (0);
+}
+
+
 
