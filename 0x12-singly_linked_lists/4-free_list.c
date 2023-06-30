ef77ca00a0ad1ac74113a6dedd37f29a135dd6e3
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-01 00:31:42 +0300

    a function that frees a list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_list - function that free a list_t
+ *
+ * @head: head of the list
+ *
+ */
+
+void free_list(list_t *head)
+{
+
+	list_t *ptr;
+
+	while (head !=  NULL)
+	{
+		ptr = head;
+
+		head = head->next;
+
+		free(ptr);
+
+	}
+
+}
