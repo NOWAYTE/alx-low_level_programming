6665f886695168346ffda1e024270a96b5346719
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-05 01:32:12 +0300

    Fixing compilation errors

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *n_node;
 
 
-	new_node = malloc(sizeof(listint_t));
+	n_node = malloc(sizeof(listint_t));
 
 	if (n_node == NULL)
 	{
@@ -24,7 +24,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	}
 
-	n_node->n;
+	n_node->n = n;
 	n_node->next = *head;
 	*head = n_node;
 
