e4b3132407c25abef4ead7782ad75b0eecd60b00
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-07 01:28:11 +0300

    Fix code errors

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * clear_bit - a function that sets the value of 
+ * clear_bit - a function that sets the value of
  * a bit to 0
  *
  * @n: pointer to integer parameter
  *
  * @index: refers to the index
  *
- * Return: returns 1 if it worked 
+ * Return: returns 1 if it worked
  *
  */
 
