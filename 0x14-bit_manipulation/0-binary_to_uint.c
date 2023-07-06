42a2b0926bbc5d5b01534efca6a145112cc49248
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-06 18:44:06 +0300

    Fix code errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,39 +10,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
-	int x = 1;
+	int x = 0;
 
 	if (!b)
 	{
 		return (0);
 	}
 
-	while (*b)
+	while (b[x] == '0' || b[x] == '1')
 	{
-		if (x <= 32)
-		{
-			if (*b == '0' || *b == '1')
-			{
-				i <<= 1;
-				i += *b - '0';
-			}
+		i <<= 1;
+		i += b[i] - '0';
 
-		}
-		else
-		{
-			return (0);
-
-		}
-
-
-		b++;
 		x++;
-
-	}
-
-	if (x > 32)
-	{
-		return (0);
 	}
 
 	return (i);
