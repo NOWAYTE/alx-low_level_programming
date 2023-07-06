1f1c471187a57ddac6970dc6b15675865ce4e43f
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-07 01:25:46 +0300

    Fix code errors

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int x = sizeof(unsigned long int) * 8
+	unsigned int x = sizeof(unsigned long int) * 8;
 
 
 	if (index >= x)
