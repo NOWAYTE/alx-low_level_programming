a5c08b4e58195e73480ece9fc57184cc71f731b3
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-06 19:02:18 +0300

    Fix code errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,33 @@
 #include "main.h"
+/**
+ * binary_to_uint - convertsa bonary number to an
+ * unsigned int
+ *
+ * @b: pointer to a string
+ *
+ * Return: returns the converted number
+ *
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int val = 0;
-    int i = 0;
+	unsigned int x = 0;
 
-    if (b == NULL)
-        return (0);
+	int i = 0;
 
-    while (b[i] == '0' || b[i] == '1')
-    {
-        val <<= 1;
-        val += b[i] - '0';
-        i++;
-    }
+	if (b == NULL)
+		return (0);
 
-    return (val);
+	while (b[i] == '0' || b[i] == '1')
+	{
+		x <<= 1;
+		x += b[i] - '0';
+
+
+		i++;
+	}
+
+	return (x);
+
 }
 
