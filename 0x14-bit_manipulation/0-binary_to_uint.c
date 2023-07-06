12ce56209a0469e280314c575fd9b81f7fcebb70
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-06 18:52:05 +0300

    Fix code error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,19 @@
 #include "main.h"
-/**
- * binary_to_uint - converts binary to number
- * @b: pointer to a string
- *
- * Return: returns the cionverted number
- *
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	int x = 0;
+    unsigned int val = 0;
+    int i = 0;
 
-	if (!b)
-	{
-		return (0);
-	}
+    if (b == NULL)
+        return (0);
 
-	while (b[x] == '0' || b[x] == '1')
-	{
-		i = (i * 2) + (*b - '0');
+    while (b[i] == '0' || b[i] == '1')
+    {
+        val <<= 1;
+        val += b[i] - '0';
+        i++;
+    }
 
-		x++;
-	}
-
-	return (i);
-
+    return (val);
 }
-
-
-
 
