9968b3616403086b386a2f3a6797efc8f2a0a30a
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-12 02:10:29 +0300

    Fix code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *pt;
 	size_t i;
 	char c;
-	ssize_t write_n;
 
 	FILE *ptr = NULL;
 
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,7 @@ int create_file(const char *filename, char *text_content)
 	int len = 0;
 	int i = 0;
 	FILE *ptr = NULL;
+	int f;
 
 	if (filename == NULL)
 	{
@@ -27,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	}
 
-	ptr = fopen(filename, "r+");
+	f = fopen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (ptr == NULL)
 	{
