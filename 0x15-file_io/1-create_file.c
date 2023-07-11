1ebd121bac801909e97b7088538db63293737782
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-12 01:36:26 +0300

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ *
+ * @filename: pointer to a file
+ *
+ * @text_content: string to write 
+ * 
+ * Return: 1 (success)
+ *
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int len = 0;
+	int i = 0;
+	FILE *ptr = NULL;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		ptr = fopen(filename, "r");
+
+	}
+
+	ptr = fopen(filename, "r+");
+
+	if (ptr == NULL)
+	{
+		return (-1);
+
+	}
+
+	for (len = 0; text_content[len] != '\0'; len++)
+	{
+		i = fwrite(text_content, len, 1, ptr);
+		if (i == -1)
+		{
+			fclose(ptr);
+			return (-1);
+
+		}
+
+	}
+
+	fclose(ptr);
+
+	return (-1);
+
+}
+
+
+
+
+
