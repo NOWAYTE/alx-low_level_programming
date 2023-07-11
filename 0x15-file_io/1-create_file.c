762e0c9dd6ef61a2ce1875f573ed4bbdfad291af
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-12 02:16:56 +0300

    Fix code errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,6 @@ int create_file(const char *filename, char *text_content)
 	int len = 0;
 	int i = 0;
 	FILE *ptr = NULL;
-	int f;
 
 	if (filename == NULL)
 	{
@@ -24,11 +23,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		ptr = fopen(filename, "r");
+		ptr = fopen(filename, "w");
 
 	}
 
-	f = fopen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	ptr = fopen(filename, "w");
 
 	if (ptr == NULL)
 	{
