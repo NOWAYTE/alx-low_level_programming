5baea54334e4fdcbc39ad669425eb2976b9270bc
Daniel kamau <danielkamaunganga@gmail.com>
2023-07-13 15:29:29 +0300

    A function that reads a text file ad prints it to the POSIX standard ouptut

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,21 +6,21 @@
  *
  * @letters: number of letters it should read
  *
- * Return: returns the actual number fo letters it could print
- *
+ * Return: returns the actual number of letters 
+ * it could read
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *pt;
-	size_t i;
-	char c;
-
 	FILE *ptr = NULL;
+	size_t read_n, write_n;
+	char *buff = NULL;
+
 
 	if (filename == NULL)
 	{
 		return (0);
+
 	}
 
 	ptr = fopen(filename, "r");
@@ -28,29 +28,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (ptr == NULL)
 	{
 		return (0);
-	}
-
-	while ((c = fgetc(ptr)) != EOF && i < letters)
-	{
-		putchar(c);
-
-		i++;
 
 	}
+	buff = malloc(sizeof(char) * letters + 1);
 
-	pt = malloc(sizeof(char) * letters);
+	read_n = fread(buff, 1,letters, ptr);
 
-	if (pt == NULL)
+	if (read_n != 1)
 	{
 		return (0);
+
 	}
 
-	free(pt);
+	write_n = write(STDOUT_FILENO, buff, 1);
 
-	fclose(ptr);
+	if (write_n != 1)
+	{
+		return (0);
 
-	return (i);
+	}
+
+	return read_n;
 
 }
 
+	
 
+
+
+	
