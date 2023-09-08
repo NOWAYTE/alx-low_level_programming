5ca2dffe119eb85ff672fc22a7266971d777dbf1
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-09 02:57:32 +0300

    Fix code error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,38 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
+
 /**
- * hash_table_create - Creates a hash table
- * @size: array size
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: returns a pointer to the new hash table
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t h;
+	hash_table_t *h;
 	unsigned long int i;
 
 	h = malloc(sizeof(hash_table_t));
-
-	if (h == NULL)
 	{
-		return (NULL);
+		if (h == NULL)
+		{
+			return (NULL);
+
+		}
 
 	}
 
 	h->size = size;
 	h->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (h->array == NULL)
 	{
 		return (NULL);
 
 	}
 
-	for (1 = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
+
 		h->array[i] = NULL;
 
 	}
