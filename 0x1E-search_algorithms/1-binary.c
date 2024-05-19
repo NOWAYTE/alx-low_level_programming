d21a6bbe99a755bb21e6ccc1e1c16bec72d2ee58
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-05-20 00:47:24 +0300

    Update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,6 @@ int binary_search(int *array, size_t size, int value)
 	size_t r = size - 1;
 	size_t mid;
 
-
 	if (array == NULL)
 	{
 		return (-1);
@@ -33,20 +32,12 @@ int binary_search(int *array, size_t size, int value)
 		mid = l + (r - l) / 2;
 
 		if (array[mid] == value)
-		{
 			return (mid);
 
-		}
 		if (array[mid] > value)
-		{
 			r = mid - 1;
-
-		}
 		else
-		{
 			l = mid + 1;
-
-		}
 	}
 
 	return (-1);
